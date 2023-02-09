ae62a8fcccf2326a1b681b3d68b44ecd6aa980b7
Michal Hoftich <michal.h21@gmail.com>
2023-02-09 14:22:59 +0100

    deduplicate mms ids

--- sysnostoxlsx.py
+++ sysnostoxlsx.py
@@ -5,9 +5,13 @@ from sys import argv
 
 script, almafile = argv
 
-# load only record IDS
+# load only record IDs
 df = almaxml.load(almafile, {'C5':'id'})
 # rename column to use the correct name in the xlsx file
 df = df.rename(columns={'id':'MMS_ID'})
+
+# this doesn't seem to do anything, but the idea is to 
+# remove duplicated IDs
+df = df.drop_duplicates(ignore_index=True)
 
 df.to_excel("mms_id.xlsx", index = False)
